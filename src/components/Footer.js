9f48433f88ebb9abf3bf13048e300fb20039217b
Camille Condrey <camillecondrey@gmail.com>
2017-07-15 11:27:45 -0600

    mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import './Footer.css';
 
 export class Footer extends Component {
   render() {
     return (
+
         <footer>
-          <p className="created-by">Created by Camille Condrey</p>
+          <div className="foot">
+            
+              <Link to="https://github.com/camillecondrey"><img src="../images/GitHub-Mark-Light-32px.png"></img></Link>
+           
+            <p className="created-by">Created by Camille Condrey</p>
+            
+              <Link to="https://www.linkedin.com/in/camille-condrey-bb65568b/"><img src="../images/In-White-34px-R.png"></img></Link>
+            
+          </div>
         </footer>
     )
   }
